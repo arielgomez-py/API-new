ea2bfa228a6d7807cce2d9d0a803e067b20efb4a
Ariel <arielgomezmendez94@gmail.com>
2022-03-27 11:46:04 -0400

    Make request to Pokemons API, obtain image of pokemon

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 
-//Request fetch to API
-fetch('https://pokeapi.co/api/v2/pokemon/1/')
-.then(response => response.json())
-.then(data => console.log(data))
-.catch(err => console.log(err))
+//Request http to Pokemons API
+const  obtainPokemons = async() =>{
+    //Request
+    try{
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+
+        //Obtain data
+        const data = await response.json();
+        arrayPokemons = data.results;
+        console.log(data);
+        let urlImage = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png";
+        
+        //obtain image of pokemon
+        let element = document.getElementById("foto").src=urlImage;
+        
+        //obtain name of pokemon
+        //let paragraph = document.getElementById("name");
+        //paragraph.innerText = data.species.name; 
+        
+       
+    }
+    
+    // error case
+    catch(error){
+        console.log(error);
+    }    
+}
+
+obtainPokemons();
+
 
